1864f604c577ed7a64cdb514a63ee19ee8439983
Alessandro Astone <ales.astone@gmail.com>
2020-12-15 01:20:29 +0100

    smith: Use libcomparetf2 shim for __lttf2
    Change-Id: I5f71840886e0ddf1fa15a40f4d56dedb3fead22e

--- device.mk
+++ device.mk
@@ -70,3 +70,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Touch
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/touch/input-port-associations.xml:$(TARGET_COPY_OUT_VENDOR)/etc/input-port-associations.xml
+
+# VNDK
+PRODUCT_PACKAGES += \
+    libcomparetf2_shim
