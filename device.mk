37df3ee8a948d93a902b96f2dd28cfb9f23e21f0
Aaron Kling <webgeek1234@gmail.com>
2023-08-28 21:17:30 -0500

    smith: Fix up proprietary files for elf checks
    Change-Id: If8dff97a016bc4574026252a714ecdccfcd948b3

--- device.mk
+++ device.mk
@@ -47,6 +47,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
+    com.motorola.hardware.biometric.fingerprint@1.0.vendor
+
 # Fold related configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fold/device_state_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/devicestate/device_state_configuration.xml \
