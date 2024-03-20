f0a3004800d68b6e534061e15d58c8f034e59eac
Andrew Hexen <SyberHexen@gmail.com>
2024-03-20 03:21:12 -0700

    smith: setup eUICC
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Ib6e8299e009c574c4cc2db07861473ae164b96f6

--- device.mk
+++ device.mk
@@ -47,6 +47,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+PRODUCT_COPY_FILES += \
+frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor \
