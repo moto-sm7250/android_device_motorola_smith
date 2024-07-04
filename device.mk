f88f338daebd2a47ec4edd9f098d405db2ad1868
Andrew Hexen <SyberHexen@gmail.com>
2024-07-04 09:42:54 -0700

    smith: add missing fast_switch_mixer_paths.xml
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Ic3db9fda5ec8a924ca9dc817824bdbbe8079e554

--- device.mk
+++ device.mk
@@ -40,6 +40,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
+    $(LOCAL_PATH)/audio/fast_switch_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/fast_switch_mixer_paths.xml \
     $(LOCAL_PATH)/audio/mixer_paths_madera_princeb0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_madera_princeb0.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
