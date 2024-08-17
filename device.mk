e497ca9c432b099fc58b2cc03049822ca5823ee3
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 04:59:30 +0300

    smith: do not manually build dependencies
    Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b

--- device.mk
+++ device.mk
@@ -48,11 +48,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1.vendor \
-    com.motorola.hardware.biometric.fingerprint@1.0.vendor
-
 # Init
 PRODUCT_PACKAGES += \
     init.mmi.laser.sh \
